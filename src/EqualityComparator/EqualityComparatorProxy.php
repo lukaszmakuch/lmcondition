b994b4fb1b677bb37b549c44f260ed776a3fe613
Łukasz Makuch <kontakt@lukaszmakuch.pl>
2015-08-10 21:46:02 +0200

    Equality comparator proxy supports registering and using actual comparators

--- src/EqualityComparator/EqualityComparatorProxy.php
+++ src/EqualityComparator/EqualityComparatorProxy.php
@@ -23,9 +23,61 @@ use \lukaszmakuch\LmCondition\Condition;
  */
 class EqualityComparatorProxy implements EqualityComparator
 {
+    const REGISTERED_CMPS_OBJ = 0;
+    const REGISTERED_CMPS_CLASS1 = 1;
+    const REGISTERED_CMPS_CLASS2 = 2;
+    
+    protected $registeredComparators;
+    
+    public function __construct()
+    {
+        $this->registeredComparators = [];
+    }
     
     public function equal(Condition $c1, Condition $c2)
     {
+        return $this->findComparatorFor($c1, $c2)->equal($c1, $c2);
+    }
+    
+    public function register(
+        EqualityComparator $actualComparator,
+        $conditionClass1,
+        $conditionClass2
+    ) {
+        $this->registeredComparators[] = [
+            self::REGISTERED_CMPS_OBJ => $actualComparator,
+            self::REGISTERED_CMPS_CLASS1 => $conditionClass1,
+            self::REGISTERED_CMPS_CLASS2 => $conditionClass2,
+        ];
+    }
+    
+    /**
+     * @param Condition $c1
+     * @param Condition $c2
+     * 
+     * @return EqualityComparator
+     */
+    protected function findComparatorFor(Condition $c1, Condition $c2)
+    {
+        foreach ($this->registeredComparators as $touple) {
+            if ($this->conditionsMatchRegisteredCmpTuple($c1, $c2, $touple)) {
+                return $touple[self::REGISTERED_CMPS_OBJ];
+            }
+        }
         throw new \InvalidArgumentException();
+    }
+    
+    protected function conditionsMatchRegisteredCmpTuple(
+        Condition $c1,
+        Condition $c2,
+        array $registeredCmpTuple
+    ) {
+        return (
+            ($c1 instanceof $registeredCmpTuple[self::REGISTERED_CMPS_CLASS1])
+            && ($c2 instanceof $registeredCmpTuple[self::REGISTERED_CMPS_CLASS2])    
+        ) || (
+            ($c2 instanceof $registeredCmpTuple[self::REGISTERED_CMPS_CLASS1])
+            && ($c1 instanceof $registeredCmpTuple[self::REGISTERED_CMPS_CLASS2])    
+        );
     }
 }
--- tests/EqualityComparator/EqualityComparatorProxyTest.php
+++ tests/EqualityComparator/EqualityComparatorProxyTest.php
@@ -9,9 +9,12 @@
 
 namespace lukaszmakuch\LmCondition\EqualityComparator;
 
+use InvalidArgumentException;
 use lukaszmakuch\LmCondition\tests\BooleanCondition;
+use lukaszmakuch\LmCondition\tests\ValueGreaterThan;
 use PHPUnit_Framework_TestCase;
-use Zend\Stdlib\Hydrator\Strategy\Exception\InvalidArgumentException;
+
+class BooleanConditionDerivative extends BooleanCondition {}
 
 class EqualityComparatorProxyTest extends PHPUnit_Framework_TestCase
 {
@@ -30,5 +33,87 @@ class EqualityComparatorProxyTest extends PHPUnit_Framework_TestCase
         $c1 = new BooleanCondition(true);
         $c2 = new BooleanCondition(true);
         $this->proxy->equal($c1, $c2);
+    }
+    
+    public function testRegisteringAndUsingComparator()
+    {
+        $actualComparator = $this->getComparatorStub(true);
+        $this->proxy->register(
+            $actualComparator, 
+            BooleanCondition::class, 
+            BooleanCondition::class
+        );
+        
+        $c1 = new BooleanCondition(true);
+        $c2 = new BooleanCondition(true);
+        
+        $actualComparator->expects($this->atLeastOnce())
+            ->method("equal")
+            ->with($c1, $c2);
+        $this->assertTrue($this->proxy->equal($c1, $c2));
+    }
+    
+    public function testRegisteringForDifferentClasses()
+    {
+        $actualComparator = $this->getComparatorStub(true);
+        $this->proxy->register(
+            $actualComparator, 
+            BooleanCondition::class, 
+            ValueGreaterThan::class
+        );
+        
+        $c1 = new BooleanCondition(true);
+        $c2 = new ValueGreaterThan(123);
+        
+        $actualComparator->expects($this->atLeastOnce())
+            ->method("equal")
+            ->with($c1, $c2);
+        
+        $this->assertTrue($this->proxy->equal($c1, $c2));
+    }
+    
+    public function testDerivatives()
+    {
+        $actualComparator = $this->getComparatorStub(true);
+        $this->proxy->register(
+            $actualComparator, 
+            BooleanCondition::class, 
+            BooleanConditionDerivative::class
+        );
+        
+        $c1 = new BooleanCondition(true);
+        $c2 = new BooleanConditionDerivative(true);
+        
+        $actualComparator->expects($this->atLeastOnce())
+            ->method("equal")
+            ->with($c1, $c2);
+        
+        $this->assertTrue($this->proxy->equal($c1, $c2));
+    }
+    
+    public function testDifferentOrderOfGivenObjects()
+    {
+        $actualComparator = $this->getComparatorStub(true);
+        $this->proxy->register(
+            $actualComparator, 
+            BooleanCondition::class, 
+            BooleanConditionDerivative::class
+        );
+        
+        $c1 = new BooleanConditionDerivative(true);
+        $c2 = new BooleanCondition(true);
+        
+        $actualComparator->expects($this->atLeastOnce())
+            ->method("equal")
+            ->with($c1, $c2);
+        
+        $this->assertTrue($this->proxy->equal($c1, $c2));
+    }
+    
+    protected function getComparatorStub($cmpResult)
+    {
+        $stub = $this->getMock(EqualityComparator::class);
+        $stub->method("equal")->willReturn($cmpResult);
+        return $stub;
     }
 }
